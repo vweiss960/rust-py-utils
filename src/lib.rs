7269f3081f64e0cf6f9a455f83b1c2b41ff14e31
vweiss <vincent.weiss@sev1tech.com>
2026-01-16 10:49:27 -0700

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+use std::io::{self, Write};
+
+pub fn input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+    
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
